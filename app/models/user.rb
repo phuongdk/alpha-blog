5494037dbde3761689f9cc640750623b33498187
Phương Đỗ <phuongdkk@gmail.com>
2017-08-21 13:36:20 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+validates :username,
+     presence: true,
+     uniqueness: { case_sensitive: false },
+     length: {minimum:3,maximum:30}
+validates :email, 
+     presence: true,
+     uniqueness: { case_sensitive: false },
+     length: {minimum:5,maximum:30},
+     format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Wrong email format" }
 end    
