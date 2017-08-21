d1f2302d72ab3385feee18bf7c8fdc949f549bd2
Phương Đỗ <phuongdkk@gmail.com>
2017-08-21 17:10:30 +0000

    add user datas to article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     end 
     
     def index
-     @articles = Article.all
+    @articles = Article.all
     end    
     
     def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
-validates :username,
+    has_many :articles
+    before_save { self.email = email.downcase }
+    validates :username,
      presence: true,
      uniqueness: { case_sensitive: false },
      length: {minimum:3,maximum:30}
-validates :email, 
+    validates :email, 
      presence: true,
      uniqueness: { case_sensitive: false },
-     length: {minimum:5,maximum:30},
+     length: {minimum:5,maximum:50},
      format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Wrong email format" }
 end    
