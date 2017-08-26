9627e675c19d10314b0280d1bcdfcbd278967f2b
Phương Đỗ <phuongdkk@gmail.com>
2017-08-26 19:20:31 +0000

    Complete admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
    before_action :set_user, only: [:edit,:update,:show,:destroy]
    before_action :require_user, except: [:index,:signup,:create]
    before_action :require_same_user, only: [:edit,:update,:destroy]
+   before_action :require_admin, only: [:index]
    def index
     @users = User.paginate(page: params[:page],per_page: 4)
    end
@@ -60,10 +61,17 @@ class UsersController < ApplicationController
    end   
    
    def require_same_user
-     if current_user != @user
+     if current_user != @user and !current_user.admin?
      flash[:danger] = "Page is not found"
      redirect_to user_path(current_user.id) 
      end  
-   end  
+   end
+   
+   def require_admin
+    if logged_in? and !current_user.admin?
+    flash[:danger] = "Page is not found"
+    redirect_to root_path
+    end 
+   end 
    
 end
